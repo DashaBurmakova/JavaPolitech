31e6a358b5a32e5ad77a2cc273b856d23703e66e
DashaBurmakova <146377593+DashaBurmakova@users.noreply.github.com>
2023-12-23 23:23:06 +0300

    3 лаба

--- Third/Third/src/Main.java
+++ Third/Third/src/Main.java
@@ -1,17 +1,33 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Логические операторы && || !
+        boolean a = true;
+        boolean b = false;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Логическое И: " + (a && b));  // false
+        System.out.println("Логическое ИЛИ: " + (a || b));  // true
+        System.out.println("Логическое НЕ: " + !a);  // false
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // Тернарная операция x < y ? x : y
+        int x = 5;
+        int y = 10;
+        int result = (x < y) ? x : y;
+        System.out.println("Тернарная операция: " + result);  // 5
+
+        // Поразрядные логические операторы & | ^ ~
+        int num1 = 7;  // 0111 в двоичной системе
+        int num2 = 3;  // 0011 в двоичной системе
+
+        System.out.println("Поразрядное И: " + (num1 & num2));  // 3
+        System.out.println("Поразрядное ИЛИ: " + (num1 | num2));  // 7
+        System.out.println("Поразрядное исключающее ИЛИ: " + (num1 ^ num2));  // 4
+        System.out.println("Поразрядное НЕ для num1: " + (~num1));  // -8
+
+        // Операции сдвига >> << >>>
+        int value = 8;  // 1000 в двоичной системе
+        System.out.println("Сдвиг вправо на одну позицию: " + (value >> 1));  // 4
+        System.out.println("Сдвиг влево на одну позицию: " + (value << 1));  // 16
+        System.out.println("Сдвиг вправо с заполнением нулями: " + (value >>> 1));  // 4
     }
 }
